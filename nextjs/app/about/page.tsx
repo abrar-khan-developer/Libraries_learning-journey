3c7479a9f830f740328ec7fd030ac052a5096c8f
abrar0349 <abrar0349khan@gmail.com>
2026-05-31 23:53:32 +0500

    Nextjs journey why need next js to learn for multipage rendering

---
+++ nextjs/app/about/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200">
+      About page
+    </h1>
+  );
+};
+
+export default Page;
---
+++ nextjs/app/page.tsx
@@ -0,0 +1,12 @@
+// import Image from "next/image"; -    @ ya ./ na ho to matlab h ky node_modules sy import ho rhi h
+ 
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200">
+        Welcome to Next.js 14 Home page!
+      </h1>
+   
+    </div>
+  );
+}
