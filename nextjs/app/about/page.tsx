75c1354af1144456819afc0c774f62aafef71fd6
abrar-khan-developer <abrarkhan031027@gmail.com>
2026-06-12 22:28:39 +0500

    "use client" use topic which is basically use for send js file to client side basically tag create and render in client side if we use "use client" in file

--- nextjs/app/about/page.tsx
+++ nextjs/app/about/page.tsx
@@ -1,6 +1,11 @@
+"use client"
+
 const Page = () => {
   return (
-    <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200">
+    <h1 
+        className="text-4xl font-bold text-gray-800 dark:text-gray-200 cursor-pointer"
+        onClick={() => alert("Hello from the About page!")}
+        >
       About page
     </h1>
   );
