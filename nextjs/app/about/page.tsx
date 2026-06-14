e4a040b5434b45db82892d9ebf930a933ff17536
abrar-khan-developer <abrarkhan031027@gmail.com>
2026-06-14 08:08:56 +0500

    next topic is state variable which tell use how to rerender the function or use return again this topic is come after handleClick function

--- nextjs/app/about/page.tsx
+++ nextjs/app/about/page.tsx
@@ -1,13 +1,28 @@
 "use client"
 
+import { useState } from "react";
+
 const Page = () => {
+  
+  const [count, setCount] = useState(2);
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  handleClick();
+
   return (
-    <h1 
+    <>
+          <h1 
         className="text-4xl font-bold text-gray-800 dark:text-gray-200 cursor-pointer"
         onClick={() => alert("Hello from the About page!")}
         >
-      About page
-    </h1>
+              About page
+          </h1>
+
+          <p> Count: {count} </p>
+    </>
+
   );
 };
 
